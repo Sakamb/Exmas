4aa1a82b813e049ccd32d8de6762675947f9ca7f
Sakamb04 <sakamb04@gmail.com>
2021-11-26 14:08:47 +0000

    positioned the image to it's right location

--- src/components/IntroPage.js
+++ src/components/IntroPage.js
@@ -3,12 +3,12 @@ import './IntroPage.css'
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
+import image1 from './resources/image1.svg';
 import icon1 from './resources/icon1.svg';
 import CloudIcon from './resources/cloudIcon.svg';
 import CardIcon from './resources/cardIcon.svg';
 import PolicyIcon from './resources/policyIcon.svg';
 import Container from '@material-ui/core/Container';
-
 import PropTypes from 'prop-types';
 
 import {
@@ -45,62 +45,63 @@ export default function RowAndColumnGap() {
   
       <div className="into-page">
     
-        <Typography Font roboto color="blue" fontWeight='bold' fontSize='28' align="center" sx={{ mt: 13, mb: 5 }} style={{ color: "#2F76B9" }}>
+        <Typography Font roboto color="blue" fontWeight='bold'  align="center" sx={{fontSize: 18, mt: 14, mb: 10 }} style={{ color: "#2F76B9" }}>
           Discover best opportunities to understand your expenses
         </Typography>
         <Box
           sx={{
             display: 'grid',
-            columnGap: 10,
+            columnGap: 0,
             rowGap: 10,
-            gridTemplateColumns: 'repeat(2, 1fr)',
+            gridTemplateColumns: 'repeat(2, 2fr)',
             mx: 4, p: 2,
           }}>
 
           <Item>
             <Typography Font roboto color="blue">
-              <img src={icon1} alt="icon1" align="left" style={{ height: "4rem", width: "4 rem" }} />
+              <img src={icon1} alt="icon1" align="left" style={{ height: "4.2rem", width: "4.2rem" }} />
             </Typography>
             <Typography component="div" Font roboto color="black" align="left" sx={{ my: 2, mx: 10 }}>
               Scan Expense
             </Typography>
-            <Typography Font poppins style={{ color: "#524646" }} align="left" sx={{ fontSize: 14, my: 1, mx: 8, pl: 2 }}>
+            <Typography Font poppins style={{ color: "#524646" }} align="left" sx={{ fontFamily: 'Poppins', fontSize: 14, my: 1, mx: 8, pl: 2 }}>
               {message}
             </Typography>
           </Item>
           <Item sx={{ mx: -30 }} >
             <Typography Font roboto color="blue">
-              <img src={PolicyIcon} alt="icon1" align="left" style={{ height: "4rem", width: "4rem" }} />
+              <img src={PolicyIcon} alt="icon1" align="left" style={{ height: "4.2rem", width: "4.2rem" }} />
             </Typography>
             <Typography component="div" Font roboto color="black" align="left" sx={{ my: 2, mx: 10 }}>
               Scan Expense
             </Typography>
-            <Typography Font poppins style={{ color: "#524646" }} align="left" sx={{ fontSize: 14, my: 1, mx: 8, pl: 2 }}>
+            <Typography Font poppins style={{ color: "#524646" }} align="left" sx={{ fontFamily: 'Poppins', fontSize: 14, my: 1, mx: 8, pl: 2 }}>
               {message}
             </Typography></Item>
           <Item> <Typography Font roboto color="blue">
-            <img src={CardIcon} alt="icon1" align="left" style={{ height: "4rem", width: "4rem" }} />
+            <img src={CardIcon} alt="icon1" align="left" style={{ height: "4.2rem", width: "4.2rem" }} />
           </Typography>
             <Typography component="div" Font roboto color="black" align="left" sx={{ my: 2, mx: 10 }}>
               Scan Expense
             </Typography>
-            <Typography Font roboto style={{ color: "#524646" }} align="left" sx={{ fontSize: 14, my: 1, mx: 8, pl: 2 }}>
+            <Typography Font roboto style={{ color: "#524646" }} align="left" sx={{  fontFamily: 'Poppins',  fontSize: 14, my: 1, mx: 8, pl: 2 }}>
               {message}
             </Typography></Item>
           <Item sx={{ mx: -30 }}>
             <Typography Font roboto color="blue">
-              <img src={CloudIcon} alt="icon1" align="left" style={{ height: "4rem", width: "4rem" }} />
+              <img src={CloudIcon} alt="icon1" align="left" style={{ height: "4.2rem", width: "4.2rem" }} />
             </Typography>
             <Typography component="div" Font roboto color="black" align="left" sx={{ my: 2, mx: 10 }}>
               Scan Expense
             </Typography>
-            <Typography Font roboto style={{ color: "#524646" }} align="left" sx={{ fontSize: 14, my: 1, mx: 8, pl: 2 }}>
+            <Typography  style={{ color: "#524646" }} align="left" sx={{ lineHeight: 'normal',fontFamily: 'Poppins', fontSize: 15, fontWeight: 'regular'  , fontStyle: 'normal' , my: 1, mx: 8, pl: 2 }}>
               {message}
             </Typography>
           </Item>
         </Box>
-  
-
+        <Item  sx={{ mx: 99, my: -72 }}>
+          <img src={image1}  alt="image1" align="left" style={{ height: "30rem", width: "30rem" }} />
+          </Item>
       </div>
   );
 }
