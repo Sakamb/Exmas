c9aea65f1549561f7a9cd2351a61a6ad73c458a6
Sakamb04 <sakamb04@gmail.com>
2021-11-29 14:34:08 +0000

    a rapid version of the feature panels

--- src/components/FeaturePage.js
+++ src/components/FeaturePage.js
@@ -1,9 +1,11 @@
 import * as React from 'react';
 import PropTypes from 'prop-types';
 import Box from '@mui/material/Box';
-import Container from '@material-ui/core/Container';
+import Container from '@material-ui/core/Container'
+import { alpha } from '@mui/material/styles';
 import './FeaturePage.css'
-
+import ErrorOutlineIcon from '@mui/icons-material/ErrorOutline';
+import NotificationImg from './resources/notificationImg.svg';
 
 function Item1(props) {
   const { sx, ...other } = props;
@@ -11,13 +13,11 @@ function Item1(props) {
       
     <Box
         sx={{
-        bgcolor: 'primary.main',
-        color: 'white',
         p: 1,
         borderRadius: 1,
         textAlign: 'center',
         fontSize: '1rem',
-        fontWeight: '700',
+        fontWeight: '700',  
         ...sx,
       }}
       {...other}
@@ -40,16 +40,160 @@ export default function FeaturePage() {
         sx={{
           display: 'grid',
           gap: 1,
-          gridTemplateColumns: 'repeat(2, 1fr)',
-          my: 15,
+          gridTemplateColumns: 'repeat(3, 1fr)',
+          my: 15,   
+          alignItems: 'left',
+          height: 500,
+          width: 500,
+        //   maxHeight: { xs: 233, md: 167 },
+        //   maxWidth: { xs: 350, md: 250 },
+          left: 256,
+          top: 160,
+          position: 'relative',
+        
         }}
       >
-        <Item1>1</Item1>
-        <Item1>2</Item1>
-        <Item1>3</Item1>
-        <Item1>4</Item1>
-        <Item1>4</Item1>
-        <Item1>4</Item1>
+        <Item1>
+        <Box sx={{
+        alignItems: 'center',
+        bgcolor: 'background.paper',
+        overflow: 'hidden',
+        borderRadius: '12px',
+        boxShadow: 1,
+        fontWeight: 'bold',
+       
+      }}
+    >
+      <Box
+        component="img"
+        sx={{
+          alignItems: 'left',
+          height: 200,
+          width: 350,
+          maxHeight: { xs: 233, md: 167 },
+          maxWidth: { xs: 350, md: 250 },
+
+        }}
+        alt="The house from the offer."
+        src={NotificationImg}
+      />
+        </Box>
+        </Item1>
+        <Item1>
+        <Box sx={{
+        alignItems: 'center',
+        bgcolor: 'background.paper',
+        overflow: 'hidden',
+        borderRadius: '12px',
+        boxShadow: 1,
+        fontWeight: 'bold',
+       
+      }}
+    >
+      <Box
+        component="img"
+        sx={{
+          alignItems: 'left',
+         
+
+        }}
+        alt="The house from the offer."
+        src={NotificationImg}
+      />
+        </Box>
+        </Item1>
+        <Item1>
+        <Box sx={{
+        alignItems: 'center',
+        bgcolor: 'background.paper',
+        overflow: 'hidden',
+        borderRadius: '12px',
+        boxShadow: 1,
+        fontWeight: 'bold',
+       
+      }}
+    >
+      <Box
+        component="img"
+        sx={{
+          alignItems: 'left',
+         
+
+        }}
+        alt="The house from the offer."
+        src={NotificationImg}
+      />
+        </Box>
+        </Item1>
+        <Item1>
+        <Box sx={{
+        alignItems: 'center',
+        bgcolor: 'background.paper',
+        overflow: 'hidden',
+        borderRadius: '12px',
+        boxShadow: 1,
+        fontWeight: 'bold',
+       
+      }}
+    >
+      <Box
+        component="img"
+        sx={{
+          alignItems: 'left',
+         
+
+        }}
+        alt="The house from the offer."
+        src={NotificationImg}
+      />
+        </Box>
+        </Item1>
+        <Item1>
+        <Box sx={{
+        alignItems: 'center',
+        bgcolor: 'background.paper',
+        overflow: 'hidden',
+        borderRadius: '12px',
+        boxShadow: 1,
+        fontWeight: 'bold',
+       
+      }}
+    >
+      <Box
+        component="img"
+        sx={{
+          alignItems: 'left',
+         
+
+        }}
+        alt="The house from the offer."
+        src={NotificationImg}
+      />
+        </Box>
+        </Item1>
+        <Item1>
+        <Box sx={{
+        alignItems: 'center',
+        bgcolor: 'background.paper',
+        overflow: 'hidden',
+        borderRadius: '12px',
+        boxShadow: 1,
+        fontWeight: 'bold',
+       
+      }}
+    >
+      <Box
+        component="img"
+        sx={{
+          alignItems: 'left',
+         
+
+        }}
+        alt="The house from the offer."
+        src={NotificationImg}
+      />
+        </Box>
+        </Item1>
       </Box>
     </div>
   );
